3903891144d1390218990faa9319b3969c78e820
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-04-15 15:59:27 +0200

    added .env and gitignore

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
