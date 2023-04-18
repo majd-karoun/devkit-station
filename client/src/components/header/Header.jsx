034658f60d7efd33b296cb21e83d534d0038bcc5
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-04-18 08:05:31 +0200

    using neu deployed backend url

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -10,13 +10,13 @@ const Header = ({ setLinks, setUpdateKey }) => {
     setFilter(e.target.innerText);
     if (e.target.innerText != "All") {
       axios
-        .get(`http://localhost:5000/resources/type/${e.target.innerText}`)
+        .get(`https://devkitstation.onrender.com/resources/type/${e.target.innerText}`)
         .then((Response) => {
           setLinks(Response.data);
         });
     } else {
       const response = axios
-        .get("http://localhost:5000/resources")
+        .get("https://devkitstation.onrender.com/resources")
         .then((response) => {
           setLinks(response.data);
         });
